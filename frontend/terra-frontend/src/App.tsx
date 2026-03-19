7f5e853933cda274d04fe811808edfda8fbd0c33
Katarzyna Potarowicz <yhfe23.potaka@folkuniversitetet.nu>
2026-03-19 13:14:45 +0100

    Initialize terra-frontend project with Tailwind CSS

---
+++ frontend/terra-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <div className='bg-amber-400 min-h-screen'>
+      <h1 className='text-3xl text-center font-bold text-red-700 p-25'>
+        Czy stronka dziala?
+      </h1>
+    </div>
+  )
+}
+
+export default App;
